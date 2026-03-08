37b22ff5229df55c9c16f1960ddd122553ae3774
aguiardafa <diego.fernandes.aguiar@gmail.com>
2026-03-08 02:51:06 -0300

    add CircuitBreaker to getTime API for improved fault tolerance

--- src/main/java/com/aguiardafa/TimeApiService.java
+++ src/main/java/com/aguiardafa/TimeApiService.java
@@ -4,6 +4,7 @@ import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.faulttolerance.CircuitBreaker;
 import org.eclipse.microprofile.faulttolerance.Fallback;
 import org.eclipse.microprofile.faulttolerance.Timeout;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
@@ -16,6 +17,12 @@ public interface TimeApiService {
     @Consumes(MediaType.APPLICATION_JSON)
     @Timeout(2000L) // Configura um timeout de 2 segundos para respostas da API
     @Fallback(fallbackMethod = "getTimeFallback") // Especifica o fallback method a ser chamado em caso de falha
+    @CircuitBreaker(
+            requestVolumeThreshold = 4, // Número mínimo de chamadas para considerar o circuito
+            failureRatio = .5, // Proporção de falhas para abrir o circuito
+            delay = 5000L, // Tempo em milissegundos para manter o circuito aberto antes de tentar fechar novamente
+            successThreshold = 2 // Número de chamadas bem-sucedidas para fechar o circuito
+    )
     String getTime();
 
     default String getTimeFallback() {
