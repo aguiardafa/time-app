a6c2cdb97ffae1917b758d1189770b7e23d4b9c8
aguiardafa <diego.fernandes.aguiar@gmail.com>
2026-03-08 02:33:25 -0300

    add timeout configuration for getTime API method

--- src/main/java/com/aguiardafa/TimeApiService.java
+++ src/main/java/com/aguiardafa/TimeApiService.java
@@ -4,6 +4,7 @@ import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.faulttolerance.Timeout;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 @RegisterRestClient(baseUri = "http://localhost:8081/time-api")
@@ -12,5 +13,6 @@ public interface TimeApiService {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
+    @Timeout(2000L) // Configura um timeout de 2 segundos para respostas da API
     String getTime();
 }
