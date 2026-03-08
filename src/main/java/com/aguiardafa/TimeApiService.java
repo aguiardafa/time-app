1b8abae59e2ac431c072fe0f480b73283d501419
aguiardafa <diego.fernandes.aguiar@gmail.com>
2026-03-08 02:41:02 -0300

    add fallback method for getTime API to handle failures

--- src/main/java/com/aguiardafa/TimeApiService.java
+++ src/main/java/com/aguiardafa/TimeApiService.java
@@ -4,6 +4,7 @@ import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.faulttolerance.Fallback;
 import org.eclipse.microprofile.faulttolerance.Timeout;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
@@ -14,5 +15,10 @@ public interface TimeApiService {
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     @Timeout(2000L) // Configura um timeout de 2 segundos para respostas da API
+    @Fallback(fallbackMethod = "getTimeFallback") // Especifica o fallback method a ser chamado em caso de falha
     String getTime();
+
+    default String getTimeFallback() {
+        return "Fallback: " + java.time.LocalDateTime.now() + "\n";
+    }
 }
